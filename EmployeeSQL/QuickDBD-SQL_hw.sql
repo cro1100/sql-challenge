83cf9a6f53cb366a6c69977db1a4922cc09b6692
Charles Ro <charlesro1@gmail.com>
2020-10-30 15:54:06 -0600

    Required portion is finished

--- EmployeeSQL/QuickDBD-SQL_hw.sql
+++ EmployeeSQL/QuickDBD-SQL_hw.sql
@@ -71,10 +71,47 @@ REFERENCES "employees" ("employee_id");
 --tables and foreign keys are now created
 
 --query to select employee information, including salaries, using a join
-select employees.employee_id, first_name, last_name, sex, salary
-from employees, salaries where 
-employees.employee_id = salaries.employee_id;
+select e.employee_id, first_name, last_name, sex, salary
+from employees as e, salaries as s 
+where e.employee_id = s.employee_id;
 
+--select the employees who were hired in 1986
 select first_name, last_name, hire_date from employees where (select date_part('year', hire_date) = 1986);
+
+--select the managers of the various departments
+select d.department_id, department_name, e.employee_id, last_name, first_name
+from departments as d, employees as e, dept_manager_junction as dmj 
+where d.department_id = dmj.department_id AND
+e.employee_id = dmj.employee_id;
+
+--select all the employees and what dept they work in
+select e.employee_id, last_name, first_name, department_name
+from employees as e, departments as d, dept_emp_junction as dej
+where e.employee_id = dej.employee_id AND
+d.department_id = dej.department_id;
+
+--select all the employees whose first name is "Hercules" and last name starts with "B"
+select first_name, last_name, sex from employees 
+where first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--select all the employees in the sales department
+select e.employee_id, last_name, first_name, department_name
+from employees as e, departments as d, dept_emp_junction as dej
+where e.employee_id = dej.employee_id AND
+dej.department_id = d.department_id AND
+department_name = 'Sales';
+
+--select all the employees in either the sales or the development departments
+select e.employee_id, last_name, first_name, department_name
+from employees as e, departments as d, dept_emp_junction as dej
+where e.employee_id = dej.employee_id AND
+dej.department_id = d.department_id AND
+(department_name = 'Sales' OR department_name = 'Development');
+
+--find out how many employees share the same last name
+select last_name, count(last_name) as Frequency 
+from employees 
+group by last_name 
+order by count(last_name) desc;
 
 
